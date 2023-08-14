357d808c36f639e8c1da01057dd1c48c4690ec19
Rana-Mudassir <mudassirnazird22@gmail.com>
2023-08-14 15:27:56 +0500

    Add migration and models

---
+++ db/migrate/20230814101515_create_aeroplanes.rb
@@ -0,0 +1,14 @@
+class CreateAeroplanes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :aeroplanes do |t|
+      t.string :name
+      t.string :model
+      t.text :description
+      t.decimal :price
+      t.decimal :booking_price
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
