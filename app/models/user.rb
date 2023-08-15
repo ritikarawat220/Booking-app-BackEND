a2dd4bb734f5d07f83b72a16228e9cbba5d537d4
FrankMugagga <frank.mugagga@gmail.com>
2023-08-15 22:05:45 +0300

    Add jti matchers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+=begin
+include Devise::JWT::RevocationStrategies::JTIMatcher
+=end
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :validatable, jwt_authenticatable, jwt_revocation_strategy: self
+
+
   has_many :reservations
   has_many :aeroplanes, through: :reservations
 end
