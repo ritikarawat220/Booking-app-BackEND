1bb8db5b652fe25de112041c0a60d67edaec32ee
FrankMugagga <frank.mugagga@gmail.com>
2023-08-15 22:29:11 +0300

    Add JTIMatcher

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
-=begin
 include Devise::JWT::RevocationStrategies::JTIMatcher
-=end
 
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :validatable, jwt_authenticatable, jwt_revocation_strategy: self
+         :validatable, jwt_authenticatable, jwt_revocation_strategy: self
 
 
   has_many :reservations
