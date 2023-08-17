1d0da27d066687363cb73b88b63566232b37e0ea
FrankMugagga <frank.mugagga@gmail.com>
2023-08-17 17:01:13 +0300

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+
+  post 'reservations/create', to: 'reservations#create'
+  post '/aeroplanes/create', to: 'aeroplanes#create'
+
   get '/current_user', to: 'current_user#index'
+
   devise_for :users, path: '', path_names: {
                                  sign_in: 'login',
                                  sign_out: 'logout',
@@ -11,4 +16,9 @@ Rails.application.routes.draw do
                        sessions: 'users/sessions',
                        registrations: 'users/registrations'
                      }
+
+resources :aeroplanes, only: [:destroy, :index, :show] do
+  resources :reservations, only: [:destroy, :index, :show, :create] do
+  end
+end
 end
