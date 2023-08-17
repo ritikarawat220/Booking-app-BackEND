59bac3dd1c2a5ea428bdcc0a5a2793037841101b
FrankMugagga <frank.mugagga@gmail.com>
2023-08-17 17:01:39 +0300

    Database migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,46 +1,55 @@
-# frozen_string_literal: true
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_815_185_139) do
-  enable_extension 'plpgsql'
+ActiveRecord::Schema[7.0].define(version: 2023_08_17_125143) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table 'aeroplanes', force: :cascade do |t|
-    t.string 'name'
-    t.string 'model'
-    t.text 'description'
-    t.decimal 'price'
-    t.decimal 'booking_price'
-    t.string 'image'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "aeroplanes", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.text "description"
+    t.decimal "price"
+    t.decimal "booking_price"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'reservations', force: :cascade do |t|
-    t.date 'reservation_date'
-    t.date 'returning_date'
-    t.string 'city'
-    t.bigint 'user_id', null: false
-    t.bigint 'aeroplane_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['aeroplane_id'], name: 'index_reservations_on_aeroplane_id'
-    t.index ['user_id'], name: 'index_reservations_on_user_id'
+  create_table "reservations", force: :cascade do |t|
+    t.date "reservation_date"
+    t.date "returning_date"
+    t.string "city"
+    t.bigint "user_id", null: false
+    t.bigint "aeroplane_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["aeroplane_id"], name: "index_reservations_on_aeroplane_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'jti', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['jti'], name: 'index_users_on_jti', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "jti", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'reservations', 'aeroplanes'
-  add_foreign_key 'reservations', 'users'
+  add_foreign_key "reservations", "aeroplanes"
+  add_foreign_key "reservations", "users"
 end
