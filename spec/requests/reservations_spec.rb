0a3423bb24ee932b6cc4152566d8852f6065dd94
Ritika Rawat <Ritikarawat220@gmail.com>
2023-08-21 15:56:42 +0530

    Update changes

--- spec/requests/reservations_spec.rb
+++ spec/requests/reservations_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Aeroplanes', type: :request do
     @aeroplane = Aeroplane.create(name: 'jet', model: 'jet1', description: 'bla', price: 12, booking_price: 12,
                                   image: 'qwer')
 
-    @reservation = Reservation.create(reservation_date: '2020/01/01', returning_date: '2021/11/11', city: 'kampala',
+    @reservation = Reservation.create(reservation_date: '2020/01/01', returning_date: '2021/11/11', city: 'kambala',
                                       user: @user, aeroplane: @aeroplane)
   end
 
@@ -64,7 +64,7 @@ RSpec.describe 'Aeroplanes', type: :request do
       @reservation_params = {
         reservation: { reservation_date: '2020/01/01',
                        returning_date: '2021/11/11',
-                       city: 'kampala',
+                       city: 'kambala',
                        user: @user,
                        aeroplane: @aeroplane }
       }
